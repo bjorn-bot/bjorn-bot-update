611fcb6b031ace772e41257a9fab0fb500cd5b75
bharding2 <b.harding@gmail.com>
2018-06-01 00:09:57 -0700

    keep info update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const updateLocations = require('./lib/update_locations');
+const updateKeepInfo = require('./lib/update_keep_info');
 
 let numPings = 0;
 
@@ -9,10 +10,10 @@ app.get('/', function (request, response) {
   numPings++;
   console.log(Date.now() + ' Ping Received');
 
-  if (numPings % 3 === 0) {
-    updateLocations();
-  }
-
+  if (numPings % 3 === 0) updateLocations();
+  if (numPings === 1) updateKeepInfo();
+  if (numPings % 11 === 0 && numPings % 3 !== 0) updateKeepInfo();
+  
   response.sendStatus(200);
 });
 
